bd6eba4b04ff8d5833fb955c84ff95b28a851e04
flash221 <rahul.vada@gmail.com>
2022-06-08 16:00:28 +0530

    Updates

---
+++ precache-manifest.732c08ff536ccec3ba2829274580bd31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3f29e61909b4c81dc521",
+    "url": "/digital-business-card/static/css/main.06b0ba63.chunk.css"
+  },
+  {
+    "revision": "3f29e61909b4c81dc521",
+    "url": "/digital-business-card/static/js/main.3f29e619.chunk.js"
+  },
+  {
+    "revision": "e0fbdf30c8545720d680",
+    "url": "/digital-business-card/static/js/1.e0fbdf30.chunk.js"
+  },
+  {
+    "revision": "2ac6fa471db2f3f58c76",
+    "url": "/digital-business-card/static/js/runtime~main.2ac6fa47.js"
+  },
+  {
+    "revision": "08dec5068b2183c5de4da435c64345fc",
+    "url": "/digital-business-card/static/media/bio-image.08dec506.png"
+  },
+  {
+    "revision": "3bf99cf3c08d98b6b48524ef4fc01aa3",
+    "url": "/digital-business-card/index.html"
+  }
+];
